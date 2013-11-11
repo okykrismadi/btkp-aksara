e795923afbe6fbfebebd56f33a05133952df3579
febriankrisna <febriankrisna@gmail.com>
2013-11-11 14:26:20 +0700

    initial

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var app = angular.module("aksaraJawa", []);
+
+app.controller("header", function($scope){
+    
+});
+
+app.controller("content", function($scope){
+    
+});
+
+app.controller("footer", function($scope){
+    
+});
